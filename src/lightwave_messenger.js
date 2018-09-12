068ce0146bef1ab04c82fcd81316afa18e2700f0
Phil Hagger <philip.hagger@gmail.com>
2018-09-12 20:12:09 +0100

    trying with different timers

--- src/lightwave_messenger.js
+++ src/lightwave_messenger.js
@@ -5,23 +5,30 @@ const port = 9760;
 const sendMessage = (udp, code, callback) => {
   const udpMessage = Buffer.from('100,!' + code);
 
-  udp.send(udpMessage, 0, udpMessage.length, port, host);
-  setTimeout(() => {
-    udp.send(udpMessage, 0, udpMessage.length, port, host);
-  }, 500);
-  udp.send(udpMessage, 0, udpMessage.length, port, host, err => {
-    // TODO: need to check the response from the listener then if it is not returned fail the request
-    if (err) {
-      callback({ success: false, message: err });
-    } else {
-      console.log(`UDP message - ${udpMessage} sent to ${host}:${port}`);
-      callback({
-        success: true,
-        message: `UDP message - ${udpMessage} sent to ${host}:${port}`
-      });
-    }
-    // udp.close();
-  });
+  udp.send(udpMessage, 0, udpMessage.length, port, host, err =>
+    callback({ success: true })
+  );
+  setTimeout(() => udp.send(udpMessage, 0, udpMessage.length, port, host), 100);
+  setTimeout(() => udp.send(udpMessage, 0, udpMessage.length, port, host), 200);
+  setTimeout(() => udp.send(udpMessage, 0, udpMessage.length, port, host), 300);
+  setTimeout(() => udp.send(udpMessage, 0, udpMessage.length, port, host), 400);
+  setTimeout(
+    () => udp.send(udpMessage, 0, udpMessage.length, port, host),
+    1000
+  );
+  //   udp.send(udpMessage, 0, udpMessage.length, port, host, err => {
+  //     // TODO: need to check the response from the listener then if it is not returned fail the request
+  //     if (err) {
+  //       callback({ success: false, message: err });
+  //     } else {
+  //       console.log(`UDP message - ${udpMessage} sent to ${host}:${port}`);
+  //       callback({
+  //         success: true,
+  //         message: `UDP message - ${udpMessage} sent to ${host}:${port}`
+  //       });
+  //     }
+  //     // udp.close();
+  //   });
 };
 
 export default sendMessage;
