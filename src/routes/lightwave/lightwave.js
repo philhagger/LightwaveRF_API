e30a52b562aa21b6d0350cc64f68a18e393cb351
Phil Hagger <philip.hagger@gmail.com>
2018-09-12 19:55:37 +0100

    - added a repeat command for the udp.send

--- src/routes/lightwave/lightwave.js
+++ src/routes/lightwave/lightwave.js
@@ -1,26 +1,33 @@
 import sendMessage from '../../lightwave_messenger';
+const udp = require('dgram').createSocket('udp4');
 
 export const register = (req, res) => {
-    sendMessage('F*p', result => {
-        res.status(200).json({success: true, message: 'Press the button on the LightwaveRF Link to complete registration'});
-    });
+  sendMessage('F*p', result => {
+    res
+      .status(200)
+      .json({
+        success: true,
+        message:
+          'Press the button on the LightwaveRF Link to complete registration'
+      });
+  });
 };
 
-export const triggerLight = ({params}, res) => {
-    let status = params.on;
+export const triggerLight = ({ params }, res) => {
+  let status = params.on;
 
-    if(status === 'dim'){
-        status = `dP${params.dim}`
-    }else if(status === 'on'){
-        status = 1;
-    }else{
-        status = 0;
-    };
+  if (status === 'dim') {
+    status = `dP${params.dim}`;
+  } else if (status === 'on') {
+    status = 1;
+  } else {
+    status = 0;
+  }
 
-    const code = `R${params.room}D${params.device}F${status}`;
+  const code = `R${params.room}D${params.device}F${status}`;
 
-    sendMessage(code, result => {
-        console.log(result)
-        res.status(200).json({success: true, 'message': result});
-    });
-}
+  sendMessage(udp, code, result => {
+    console.log(result);
+    res.status(200).json({ success: true, message: result });
+  });
+};
